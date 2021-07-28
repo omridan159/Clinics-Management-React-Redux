5c40ac1c3c8f57d9ee1cfea17f7ada3bfdfdb46c
OmriDan <omridan159@gmail.com>
2021-07-28 14:32:10 +0300

    Add the search and update feature

--- src/components/table.js
+++ src/components/table.js
@@ -1,38 +1,55 @@
-import React from 'react';
+import React, { useState, useCallback, useEffect } from 'react';
 import ReactDataGrid from '@inovua/reactdatagrid-enterprise';
 import '@inovua/reactdatagrid-enterprise/index.css';
-import { useSelector, useDispatch } from 'react-redux'
-
-
-const columns = [
-   { name: 'HMOname', header: 'HMOname', minWidth: 50, defaultFlex: 1 },
-   { name: 'ID', header: 'ID', minWidth: 50, defaultFlex: 1 },
-   { name: 'Name', header: 'Name', minWidth: 50, defaultFlex: 1 },
-   { name: 'X', header: 'X', minWidth: 50, defaultFlex: 1 },
-   { name: 'Y', header: 'Y', minWidth: 50, defaultFlex: 1 },
-   { name: 'Z', header: 'Z', minWidth: 50, defaultFlex: 1 },
-   { name: 'address', header: 'address', minWidth: 50, defaultFlex: 1 },
-   { name: 'neighborhood', header: 'neighborhood', minWidth: 50, defaultFlex: 1 },
-];
+import { columns } from '../constants/table';
+import { updateClinicsData } from '../store/slices/clinicsDataSlice';
+import { useSelector, useDispatch } from 'react-redux';
 
 const gridStyle = { minHeight: 550 };
 
 const Table = () => {
+   const [serach, setSearch] = useState('');
+   const [clinicsData, setClinicsData] = useState(null);
+   const dispatch = useDispatch();
 
-    const data = useSelector((state) => state.clinicsData.clinicsData);
+
+
+   const onEditComplete = useCallback(({ value, columnId, rowIndex }) => {
+      dispatch(updateClinicsData({ value, columnId, rowIndex }));
+   }, []);
+
+   let data = useSelector((state) => state.clinicsData.data);
+
+   useEffect(() => {
+      if(serach){
+         const filterData = data.filter((clinic) => {
+            return clinic.HMOname.includes(serach) || clinic.neighborhood.includes(serach) ;
+         });
+         setClinicsData(filterData);
+      }
+
+   }, [serach]);
+
+   const handleChange = (e) => {
+      console.log(data);
+      setSearch(e.target.value);
+   };
+
+
 
    return (
       <>
          <div>
-            <h1>123</h1>
-            <h1>123</h1>
-            <input></input>
-            <input></input>
+            <h1>Clinics Management Tool</h1>
+            <h4>Search Now</h4>
+            <input name='search' onChange={handleChange} />
          </div>
          <ReactDataGrid
             idProperty='id'
+            onEditComplete={onEditComplete}
+            editable
             columns={columns}
-            dataSource={data}
+            dataSource={clinicsData ? clinicsData : data}
             style={gridStyle}
          />
       </>
---
+++ src/constants/table.js
@@ -0,0 +1,17 @@
+const columns = [
+   { name: 'HMOname', header: 'HMOname', minWidth: 50, defaultFlex: 1 },
+   { name: 'ID', header: 'ID', minWidth: 50, defaultFlex: 1 },
+   { name: 'Name', header: 'Name', minWidth: 50, defaultFlex: 1 },
+   { name: 'X', header: 'X', minWidth: 50, defaultFlex: 1 },
+   { name: 'Y', header: 'Y', minWidth: 50, defaultFlex: 1 },
+   { name: 'Z', header: 'Z', minWidth: 50, defaultFlex: 1 },
+   { name: 'address', header: 'address', minWidth: 50, defaultFlex: 1 },
+   {
+      name: 'neighborhood',
+      header: 'neighborhood',
+      minWidth: 50,
+      defaultFlex: 1,
+   },
+];
+
+export { columns };
